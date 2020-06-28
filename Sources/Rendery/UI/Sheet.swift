bb7a0dee8ba68c658d0fa154d61f4b7cc145073e
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-06-28 23:05:32 +0200

    Add a background color property to sheet views

--- Sources/Rendery/UI/Sheet.swift
+++ Sources/Rendery/UI/Sheet.swift
@@ -9,9 +9,18 @@ public struct Sheet<Subview>: View where Subview: View {
 
   private var _subview: Subview
 
+  private var _backgroundColor: Color = Color(red: 0, green: 0, blue: 0, alpha: 127)
+
+  public func backgroundColor(_ color: Color) -> Sheet {
+    var newSheet = self
+    newSheet._backgroundColor = color
+    return newSheet
+  }
+
   public func render(into renderer: inout ViewRenderer) {
-//    renderer.draw(mesh: ViewRenderer.rectangle)
-    renderer.draw(rectangle: Rectangle(origin: .zero, dimensions: _dimensions), color: .purple)
+    renderer.draw(
+      rectangle: Rectangle(origin: .zero, dimensions: _dimensions),
+      color: _backgroundColor)
 
     _subview.render(into: &renderer)
   }
