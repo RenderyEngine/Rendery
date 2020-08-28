836ee9bf7aa099f52e29957d6494706d9b23ef28
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-08-28 13:52:33 +0200

    Rewrite the default shader for the new render pipeline

--- Sources/Rendery/Resources/GLSL/GLSLStandardProgram.swift
+++ Sources/Rendery/Resources/GLSL/GLSLStandardProgram.swift
@@ -12,21 +12,21 @@ public struct GLSLStandardProgram: GLSLProgramDelegate {
     let parameters = context.assumingMemoryBound(to: Model.ColorPassContext.self).pointee
 
     // Bind the scene's ambient light.
-    program.assign(color: parameters.ambient, at: "ambientLight")
+    program.assign(color: parameters.ambient, at: "u_ambientLight")
 
     // Bind the positional lights' configuration.
     // FIXME: Use an array.
     if let node = parameters.lighteners.first {
-      program.assign(integer: 1, at: "light.enabled")
-      program.assign(color: node.light!.color, at: "light.color")
-      program.assign(vector3: node.sceneTranslation, at: "light.position")
+      program.assign(integer: 1, at: "lights[0].enabled")
+      program.assign(color: node.light!.color, at: "lights[0].color")
+      program.assign(vector3: node.sceneTranslation, at: "lights[0].position")
     } else {
-      program.assign(integer: 0, at: "light.enabled")
+      program.assign(integer: 0, at: "lights[0].enabled")
     }
 
     // Bind the mesh's material properties.
-    program.assign(property: parameters.material.diffuse, at: "diffuseProperty")
-    program.assign(property: parameters.material.multiply, at: "multiplyProperty")
+    parameters.material.diffuse.assign(to: "material.diffuse", textureUnit: 0, in: program)
+    parameters.material.multiply.assign(to: "material.multiply", textureUnit: 1, in: program)
 
     // Bind the transformation matrices.
     program.assign(matrix4: parameters.modelMatrix, at: "model")
@@ -38,51 +38,38 @@ public struct GLSLStandardProgram: GLSLProgramDelegate {
 
 }
 
-private extension GLSLProgram {
-
-  /// Assigns a material property at the specified location.
-  func assign(property: Material.Property, at location: String) {
-    switch property {
-    case .color(let color):
-      assign(boolean: true, at: "\(location).isColor")
-      assign(color: color, at: "\(location).color", discardingAlpha: false)
-
-    case .texture(let texture):
-      assign(boolean: false, at: "\(location).isColor")
-      assign(texture: texture, to: "\(location).texture", at: 0)
-    }
-  }
-
-}
-
 private let _vertexSource = """
 #version 330 core
-layout (location = 0) in vec3 vertexPosition;
-layout (location = 1) in vec3 vertexNormal;
-layout (location = 2) in vec2 vertexUVs;
+layout (location = 0) in vec3 i_position;
+layout (location = 1) in vec3 i_normal;
+layout (location = 2) in vec2 i_texcoords;
 
-uniform mat4 model;
-uniform mat4 mvp;
-uniform mat3 normalMatrix;
+uniform mat4 u_modelMatrix;
+uniform mat4 u_modelViewProjMatrix;
+uniform mat3 u_normalMatrix;
 
-out vec3 fragmentPosition;
-out vec3 fragmentNormal;
-out vec2 fragmentUVs;
+out VertexData {
+  vec3 position;
+  vec3 normal;
+  vec2 texcoords;
+} o;
 
 void main() {
-  fragmentPosition = vec3(model * vec4(vertexPosition, 1.0));
-  fragmentNormal = normalMatrix * vertexNormal;
-  fragmentUVs = vertexUVs;
+  o.position = vec3(u_modelMatrix * vec4(i_position, 1.0));
+  o.normal = u_normalMatrix * i_normal;
+  o.texcoords = i_texcoords;
 
-  gl_Position = mvp * vec4(vertexPosition, 1.0);
+  gl_Position = u_modelViewProjMatrix * vec4(i_position, 1.0);
 }
 """
 
 private let _fragmentSource = """
 #version 330 core
-in vec3 fragmentPosition;
-in vec3 fragmentNormal;
-in vec2 fragmentUVs;
+in VertexData {
+  vec3 position;
+  vec3 normal;
+  vec2 texcoords;
+} i;
 
 struct Light {
   bool enabled;
@@ -90,38 +77,41 @@ struct Light {
   vec3 position;
 };
 
+uniform vec3 u_ambientLight;
+uniform Light u_pointLights[1];
+uniform int u_pointLightCount;
+
 struct MaterialProperty {
-  bool isColor;
   vec4 color;
   sampler2D texture;
 };
 
-uniform vec3 ambientLight;
-uniform Light light;
-
-uniform MaterialProperty diffuseProperty;
-uniform MaterialProperty multiplyProperty;
+uniform MaterialProperty u_diffuse;
+uniform MaterialProperty u_multiply;
 
 out vec4 finalColor;
 
 void main() {
-  vec4 diffuse = diffuseProperty.isColor
-    ? diffuseProperty.color
-    : texture(diffuseProperty.texture, fragmentUVs);
-  if (diffuse.a < 0.1) {
+  // Compute the contribution of each material property.
+  vec4 diffuseColor = u_diffuse.color * texture(u_diffuse.texture, i.texcoords);
+  if (diffuseColor.a < 0.1) {
     discard;
   }
 
-  vec3 normal = normalize(fragmentNormal);
-  vec3 lightDirection = normalize(light.position - fragmentPosition);
-  float diff = max(dot(normal, lightDirection), 0.0);
-  vec3 pointLight = diff * light.color.rgb;
+  vec4 multiplyColor = u_multiply.color * texture(u_multiply.texture, i.texcoords);
 
-  vec4 texelColor = diffuse * vec4(pointLight + ambientLight, 1.0);
-  if (multiplyProperty.isColor) {
-    finalColor = texelColor * multiplyProperty.color;
-  } else {
-    finalColor = texelColor * texture(multiplyProperty.texture, fragmentUVs);
+  // Compute lighting.
+  vec3 lightColor = vec3(0.0, 0.0, 0.0);
+
+  for (int index = 0; index < u_pointLightCount; ++index) {
+    vec3 direction = normalize(u_pointLights[index].position - i.position);
+    float diff = max(dot(i.normal, direction), 0.0);
+    lightColor = lightColor + diff * u_pointLights[index].color;
   }
+
+  lightColor = lightColor + u_ambientLight;
+
+  // Compute the final color.
+  finalColor = diffuseColor * multiplyColor * vec4(lightColor, 1.0);
 }
 """
