21ff87d30ab7d0d6a7d1a7de13ab4eda858588ec
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-09-08 17:33:22 +0200

    Add support to configure different types of lights

--- Sources/Rendery/Light.swift
+++ Sources/Rendery/Light.swift
@@ -15,7 +15,23 @@ public struct Light {
   public enum LightingType {
 
     /// An omni-directional light source that spreads from a single point.
+    ///
+    /// The light is emitted from the position of the node to which it is attached. Rotation and
+    /// scale do not affect a point light's behavior.
     case point
+
+    /// A light source that spreads in a cone-shaped beam.
+    ///
+    /// The light is emitted from the position of the node to which it is attached, and spreads in
+    /// the direction of the node's rotation. Scale does not affect a spot light's behavior.
+    case spot
+
+    /// An infinitely far directional light source that emits light in a single direction.
+    ///
+    /// Directional lights are typically used to simulate large and distant light emitters, such as
+    /// the sun or the moon. The light's direction is defined by the rotation of the node to which
+    /// it is attached. Translation and scale do not affect a directional light's behavior.
+    case directional
 
   }
 
@@ -25,6 +41,18 @@ public struct Light {
   public var color: Color = .white
 
   /// The light's intensity.
-  public var intensity: Double = 1.0
+  public var intensity = 1.0
+
+  /// The light's range.
+  ///
+  /// For a point light, this property defines the radius of a sphere delimiting the area being
+  /// lit. Similarly, it defines the length of a cone for a spot light.
+  public var range: Double = 10.0
+
+  /// The light's angle.
+  ///
+  /// This property is only relevant for a spot light, and defines the width of a cone delimiting
+  /// the area being lit.
+  public var angle: Angle = .deg(45.0)
 
 }
