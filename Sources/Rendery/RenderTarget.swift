984fe20bd827b5df9512d970c962272daabdb9a4
Dimitri Racordon <dimitri.racordon@gmail.com>
2020-09-13 00:20:13 +0200

    Add documentation

--- Sources/Rendery/RenderTarget.swift
+++ Sources/Rendery/RenderTarget.swift
@@ -1,6 +1,9 @@
 /// An object that can receive the result of rendering operations.
 ///
 /// A render target is a sort of "canvas" onto which one can draw any kind of content.
+///
+/// Do not declare new conformances to `RenderTarget`; you will not be able to use them to render
+/// anything with Rendery. Only the `Window` and `FrameBuffer` types are valid conforming types.
 public protocol RenderTarget: AnyObject {
 
   /// The target's width, in pixels.
