e3ce9d1da3aa870cec254e3ea2432064fe1b4641
Patrick  Alexandre Sardinha <patrick.sardinha@etu.unige.ch>
2021-02-27 16:24:47 +0100

    update GLFW for Linux

--- Sources/CGLFW/shim.h
+++ Sources/CGLFW/shim.h
@@ -1,9 +1,16 @@
 // Don't load OpenGL
 #define GLFW_INCLUDE_NONE
 
+#if __APPLE__
 // Include GLFW's header from the system's location (e.g., `/usr/local/include`). The proper search
 // path is provided by a `.pc` file parsed by Swift PM.
 #include "GLFW/glfw3.h"
 
 #include <OpenGL/gl3.h>
 #include <OpenGL/gl3ext.h>
+#else
+#include <glad/glad.h>
+#include "GLFW/glfw3.h"
+#include <GL/gl.h>
+#include <GL/glext.h>
+#endif
